5cf254ff38794d33e5e443e73024034a854bb1ef
Cyber Power <cyber.power.usafa@gmail.com>
2018-03-07 14:44:01 -0700

    Fault control GUI test version complete. All phase and bolted fault function properly.

--- FaultControlTest/main.py
+++ FaultControlTest/main.py
@@ -62,11 +62,29 @@ class App(QWidget):
         self.menu_btn_phaseAC.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
         self.phaseAC_btn_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
         self.phaseAC_btn_reset.clicked.connect(self.menu_btn_reset_clicked)
+        self.phaseAC_btn_reset.clicked.connect(self.menu_btn_reset_clicked)
+        self.phaseAC_btn_relay1.clicked.connect(self.phaseAC_btn_relay1_clicked)
+        self.phaseAC_btn_relay2.clicked.connect(self.phaseAC_btn_relay2_clicked)
+        self.phaseAC_btn_relay3.clicked.connect(self.phaseAC_btn_relay3_clicked)
+        self.phaseAC_btn_relay4.clicked.connect(self.phaseAC_btn_relay4_clicked)
+        self.phaseAC_btn_relay5.clicked.connect(self.phaseAC_btn_relay5_clicked)
+        self.phaseAC_btn_relay6.clicked.connect(self.phaseAC_btn_relay6_clicked)
+        self.phaseAC_btn_relay7.clicked.connect(self.phaseAC_btn_relay7_clicked)
+        self.phaseAC_btn_relay8.clicked.connect(self.phaseAC_btn_relay8_clicked)
 
         # Phase BC menu functions
         self.menu_btn_phaseBC.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(4))
         self.phaseBC_btn_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
         self.phaseBC_btn_reset.clicked.connect(self.menu_btn_reset_clicked)
+        self.phaseBC_btn_reset.clicked.connect(self.menu_btn_reset_clicked)
+        self.phaseBC_btn_relay1.clicked.connect(self.phaseBC_btn_relay1_clicked)
+        self.phaseBC_btn_relay2.clicked.connect(self.phaseBC_btn_relay2_clicked)
+        self.phaseBC_btn_relay3.clicked.connect(self.phaseBC_btn_relay3_clicked)
+        self.phaseBC_btn_relay4.clicked.connect(self.phaseBC_btn_relay4_clicked)
+        self.phaseBC_btn_relay5.clicked.connect(self.phaseBC_btn_relay5_clicked)
+        self.phaseBC_btn_relay6.clicked.connect(self.phaseBC_btn_relay6_clicked)
+        self.phaseBC_btn_relay7.clicked.connect(self.phaseBC_btn_relay7_clicked)
+        self.phaseBC_btn_relay8.clicked.connect(self.phaseBC_btn_relay8_clicked)
 
 
     @pyqtSlot()
@@ -208,6 +226,139 @@ class App(QWidget):
         # Turn on relay 8 for bank 3
         tx = [170, 3, 254, 115, 3, 33]
         serial_transmit(tx)
+    # END Phase A/B Definitions
+
+    # BEGIN Phase A/C Definitions
+    def phaseAC_btn_relay1_clicked(self):
+        # Turn on relay 1 for bank 1
+        tx = [170, 3, 254, 108, 1, 24]
+        serial_transmit(tx)
+        # Turn on relay 1 for bank 4
+        tx = [170, 3, 254, 108, 4, 27]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay2_clicked(self):
+        # Turn on relay 2 for bank 1
+        tx = [170, 3, 254, 109, 1, 25]
+        serial_transmit(tx)
+        # Turn on relay 2 for bank 4
+        tx = [170, 3, 254, 109, 4, 28]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay3_clicked(self):
+        # Turn on relay 3 for bank 1
+        tx = [170, 3, 254, 110, 1, 26]
+        serial_transmit(tx)
+        # Turn on relay 3 for bank 4
+        tx = [170, 3, 254, 110, 4, 29]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay4_clicked(self):
+        # Turn on relay 4 for bank 1
+        tx = [170, 3, 254, 111, 1, 27]
+        serial_transmit(tx)
+        # Turn on relay 4 for bank 4
+        tx = [170, 3, 254, 111, 4, 30]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay5_clicked(self):
+        # Turn on relay 5 for bank 1
+        tx = [170, 3, 254, 112, 1, 28]
+        serial_transmit(tx)
+        # Turn on relay 5 for bank 4
+        tx = [170, 3, 254, 112, 4, 31]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay6_clicked(self):
+        # Turn on relay 6 for bank 1
+        tx = [170, 3, 254, 113, 1, 29]
+        serial_transmit(tx)
+        # Turn on relay 6 for bank 4
+        tx = [170, 3, 254, 113, 4, 32]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay7_clicked(self):
+        # Turn on relay 7 for bank 1
+        tx = [170, 3, 254, 114, 1, 30]
+        serial_transmit(tx)
+        # Turn on relay 7 for bank 4
+        tx = [170, 3, 254, 114, 4, 33]
+        serial_transmit(tx)
+
+    def phaseAC_btn_relay8_clicked(self):
+        # Turn on relay 8 for bank 1
+        tx = [170, 3, 254, 115, 1, 31]
+        serial_transmit(tx)
+        # Turn on relay 8 for bank 4
+        tx = [170, 3, 254, 115, 4, 34]
+        serial_transmit(tx)
+    # END Phase A/C Definitions
+
+    # BEGIN Phase B/C Definitions
+    def phaseBC_btn_relay1_clicked(self):
+        # Turn on relay 1 for bank 3
+        tx = [170, 3, 254, 108, 3, 26]
+        serial_transmit(tx)
+        # Turn on relay 1 for bank 4
+        tx = [170, 3, 254, 108, 4, 27]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay2_clicked(self):
+        # Turn on relay 2 for bank 3
+        tx = [170, 3, 254, 109, 3, 27]
+        serial_transmit(tx)
+        # Turn on relay 2 for bank 4
+        tx = [170, 3, 254, 109, 4, 28]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay3_clicked(self):
+        # Turn on relay 3 for bank 3
+        tx = [170, 3, 254, 110, 3, 28]
+        serial_transmit(tx)
+        # Turn on relay 3 for bank 4
+        tx = [170, 3, 254, 110, 4, 29]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay4_clicked(self):
+        # Turn on relay 4 for bank 3
+        tx = [170, 3, 254, 111, 3, 29]
+        serial_transmit(tx)
+        # Turn on relay 4 for bank 4
+        tx = [170, 3, 254, 111, 4, 30]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay5_clicked(self):
+        # Turn on relay 5 for bank 3
+        tx = [170, 3, 254, 112, 3, 30]
+        serial_transmit(tx)
+        # Turn on relay 5 for bank 4
+        tx = [170, 3, 254, 112, 4, 31]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay6_clicked(self):
+        # Turn on relay 6 for bank 3
+        tx = [170, 3, 254, 113, 3, 31]
+        serial_transmit(tx)
+        # Turn on relay 6 for bank 4
+        tx = [170, 3, 254, 113, 4, 32]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay7_clicked(self):
+        # Turn on relay 7 for bank 3
+        tx = [170, 3, 254, 114, 3, 32]
+        serial_transmit(tx)
+        # Turn on relay 7 for bank 4
+        tx = [170, 3, 254, 114, 4, 33]
+        serial_transmit(tx)
+
+    def phaseBC_btn_relay8_clicked(self):
+        # Turn on relay 8 for bank 3
+        tx = [170, 3, 254, 115, 3, 33]
+        serial_transmit(tx)
+        # Turn on relay 8 for bank 4
+        tx = [170, 3, 254, 115, 4, 34]
+        serial_transmit(tx)
+    # END Phase B/C Definitions
 
 
 app = QApplication(sys.argv)
