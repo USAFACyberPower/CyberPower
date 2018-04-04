2adc2cf3e6eea8c252d0333cc5928c3d427e694e
Cyber Power <cyber.power.usafa@gmail.com>
2018-04-03 21:46:40 -0600

    Attempting comboBox python scripts.  If issue is not resolved, buttons will have to be used instead.

--- FaultControlGUI/Rev2.1/main.py
+++ FaultControlGUI/Rev2.1/main.py
@@ -26,7 +26,7 @@ def serial_transmit(tx):
 class App(QWidget):
     def __init__(self):
         super(App, self).__init__()
-        self.ui = loadUi('FaultControlGUI.ui', self)
+        self.ui = loadUi('FaultControlGUI_Rev2.1.ui', self)
 
         # One Line Main Menu Functions
         self.ui.stackedWidget.setCurrentIndex(0)
@@ -41,151 +41,41 @@ class App(QWidget):
         self.btn_OneLine_loc1TL1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
         self.btn_loc1_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
         self.btn_loc1_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 1 - A Phase Resistive Faults
+        self.ui.stackedWidget_Loc1.setCurrentIndex(0)
+
+        self.ui.comboBox_Loc1.activated[str].connect(self.phase_select_loc1)
+        # self.ui.comboBox_Loc1.activated[str].connect(lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(0))
+        # self.ui.comboBox_Loc1.activated[str].connect(lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(1))
+        # self.ui.comboBox_Loc1.activated[str].connect(lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(2))
+        # self.ui.comboBox_Loc1.activated[str].connect(lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(3))
+
+        # Fault Location 1 - A Phase Faults
         self.btn_loc1_A200.clicked.connect(self.btn_loc1_A200_clicked)
         self.btn_loc1_A100.clicked.connect(self.btn_loc1_A100_clicked)
         self.btn_loc1_A67.clicked.connect(self.btn_loc1_A67_clicked)
         self.btn_loc1_A50.clicked.connect(self.btn_loc1_A50_clicked)
-        # Fault Location 1 - B Phase Resistive Faults
+        self.btn_loc1_Abolt.clicked.connect(self.btn_loc1_Abolt_clicked)
+
+        # Fault Location 1 - B Phase Faults
         self.btn_loc1_B200.clicked.connect(self.btn_loc1_B200_clicked)
         self.btn_loc1_B100.clicked.connect(self.btn_loc1_B100_clicked)
         self.btn_loc1_B67.clicked.connect(self.btn_loc1_B67_clicked)
         self.btn_loc1_B50.clicked.connect(self.btn_loc1_B50_clicked)
-        # Fault Location 1 - C Phase Resistive Faults
+        self.btn_loc1_Bbolt.clicked.connect(self.btn_loc1_Bbolt_clicked)
+
+        # Fault Location 1 - C Phase Faults
         self.btn_loc1_C200.clicked.connect(self.btn_loc1_C200_clicked)
         self.btn_loc1_C100.clicked.connect(self.btn_loc1_C100_clicked)
         self.btn_loc1_C67.clicked.connect(self.btn_loc1_C67_clicked)
         self.btn_loc1_C50.clicked.connect(self.btn_loc1_C50_clicked)
+        self.btn_loc1_Cbolt.clicked.connect(self.btn_loc1_Cbolt_clicked)
+
         # Fault Location 1 - Multi-Phase Faults
         self.btn_loc1_AB.clicked.connect(self.btn_loc1_AB_clicked)
         self.btn_loc1_AC.clicked.connect(self.btn_loc1_AC_clicked)
         self.btn_loc1_BC.clicked.connect(self.btn_loc1_BC_clicked)
-        self.btn_loc1_bolt.clicked.connect(self.btn_loc1_bolt_clicked)
-
-        # Fault Location 2 - Menu Functions
-        self.btn_OneLine_loc2TL5.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
-        self.btn_loc2_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.btn_loc2_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 2 - A Phase Resistive Faults
-        self.btn_loc2_A200.clicked.connect(self.btn_loc2_A200_clicked)
-        self.btn_loc2_A100.clicked.connect(self.btn_loc2_A100_clicked)
-        self.btn_loc2_A67.clicked.connect(self.btn_loc2_A67_clicked)
-        self.btn_loc2_A50.clicked.connect(self.btn_loc2_A50_clicked)
-        # Fault Location 2 - B Phase Resistive Faults
-        self.btn_loc2_B200.clicked.connect(self.btn_loc2_B200_clicked)
-        self.btn_loc2_B100.clicked.connect(self.btn_loc2_B100_clicked)
-        self.btn_loc2_B67.clicked.connect(self.btn_loc2_B67_clicked)
-        self.btn_loc2_B50.clicked.connect(self.btn_loc2_B50_clicked)
-        # Fault Location 2 - C Phase Resistive Faults
-        self.btn_loc2_C200.clicked.connect(self.btn_loc2_C200_clicked)
-        self.btn_loc2_C100.clicked.connect(self.btn_loc2_C100_clicked)
-        self.btn_loc2_C67.clicked.connect(self.btn_loc2_C67_clicked)
-        self.btn_loc2_C50.clicked.connect(self.btn_loc2_C50_clicked)
-        # Fault Location 2 - Multi-Phase Faults
-        self.btn_loc2_AB.clicked.connect(self.btn_loc2_AB_clicked)
-        self.btn_loc2_AC.clicked.connect(self.btn_loc2_AC_clicked)
-        self.btn_loc2_BC.clicked.connect(self.btn_loc2_BC_clicked)
-        self.btn_loc2_bolt.clicked.connect(self.btn_loc2_bolt_clicked)
-
-        # Fault Location 3 - Menu Functions
-        self.btn_OneLine_loc3TL4.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
-        self.btn_loc3_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.btn_loc3_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 3 - A Phase Resistive Faults
-        self.btn_loc3_A200.clicked.connect(self.btn_loc3_A200_clicked)
-        self.btn_loc3_A100.clicked.connect(self.btn_loc3_A100_clicked)
-        self.btn_loc3_A67.clicked.connect(self.btn_loc3_A67_clicked)
-        self.btn_loc3_A50.clicked.connect(self.btn_loc3_A50_clicked)
-        # Fault Location 3 - B Phase Resistive Faults
-        self.btn_loc3_B200.clicked.connect(self.btn_loc3_B200_clicked)
-        self.btn_loc3_B100.clicked.connect(self.btn_loc3_B100_clicked)
-        self.btn_loc3_B67.clicked.connect(self.btn_loc3_B67_clicked)
-        self.btn_loc3_B50.clicked.connect(self.btn_loc3_B50_clicked)
-        # Fault Location 3 - C Phase Resistive Faults
-        self.btn_loc3_C200.clicked.connect(self.btn_loc3_C200_clicked)
-        self.btn_loc3_C100.clicked.connect(self.btn_loc3_C100_clicked)
-        self.btn_loc3_C67.clicked.connect(self.btn_loc3_C67_clicked)
-        self.btn_loc3_C50.clicked.connect(self.btn_loc3_C50_clicked)
-        # Fault Location 3 - Multi-Phase Faults
-        self.btn_loc3_AB.clicked.connect(self.btn_loc3_AB_clicked)
-        self.btn_loc3_AC.clicked.connect(self.btn_loc3_AC_clicked)
-        self.btn_loc3_BC.clicked.connect(self.btn_loc3_BC_clicked)
-        self.btn_loc3_bolt.clicked.connect(self.btn_loc3_bolt_clicked)
-
-        # Fault Location 4 - Menu Functions
-        self.btn_OneLine_loc4TL7.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(4))
-        self.btn_loc4_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.btn_loc4_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 4 - A Phase Resistive Faults
-        self.btn_loc4_A200.clicked.connect(self.btn_loc4_A200_clicked)
-        self.btn_loc4_A100.clicked.connect(self.btn_loc4_A100_clicked)
-        self.btn_loc4_A67.clicked.connect(self.btn_loc4_A67_clicked)
-        self.btn_loc4_A50.clicked.connect(self.btn_loc4_A50_clicked)
-        # Fault Location 4 - B Phase Resistive Faults
-        self.btn_loc4_B200.clicked.connect(self.btn_loc4_B200_clicked)
-        self.btn_loc4_B100.clicked.connect(self.btn_loc4_B100_clicked)
-        self.btn_loc4_B67.clicked.connect(self.btn_loc4_B67_clicked)
-        self.btn_loc4_B50.clicked.connect(self.btn_loc4_B50_clicked)
-        # Fault Location 4 - C Phase Resistive Faults
-        self.btn_loc4_C200.clicked.connect(self.btn_loc4_C200_clicked)
-        self.btn_loc4_C100.clicked.connect(self.btn_loc4_C100_clicked)
-        self.btn_loc4_C67.clicked.connect(self.btn_loc4_C67_clicked)
-        self.btn_loc4_C50.clicked.connect(self.btn_loc4_C50_clicked)
-        # Fault Location 4 - Multi-Phase Faults
-        self.btn_loc4_AB.clicked.connect(self.btn_loc4_AB_clicked)
-        self.btn_loc4_AC.clicked.connect(self.btn_loc4_AC_clicked)
-        self.btn_loc4_BC.clicked.connect(self.btn_loc4_BC_clicked)
-        self.btn_loc4_bolt.clicked.connect(self.btn_loc4_bolt_clicked)
-
-        # Fault Location 5 - Menu Functions
-        self.btn_OneLine_loc5TL9.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(5))
-        self.btn_loc5_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.btn_loc5_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 5 - A Phase Resistive Faults
-        self.btn_loc5_A200.clicked.connect(self.btn_loc5_A200_clicked)
-        self.btn_loc5_A100.clicked.connect(self.btn_loc5_A100_clicked)
-        self.btn_loc5_A67.clicked.connect(self.btn_loc5_A67_clicked)
-        self.btn_loc5_A50.clicked.connect(self.btn_loc5_A50_clicked)
-        # Fault Location 5 - B Phase Resistive Faults
-        self.btn_loc5_B200.clicked.connect(self.btn_loc5_B200_clicked)
-        self.btn_loc5_B100.clicked.connect(self.btn_loc5_B100_clicked)
-        self.btn_loc5_B67.clicked.connect(self.btn_loc5_B67_clicked)
-        self.btn_loc5_B50.clicked.connect(self.btn_loc5_B50_clicked)
-        # Fault Location 5 - C Phase Resistive Faults
-        self.btn_loc5_C200.clicked.connect(self.btn_loc5_C200_clicked)
-        self.btn_loc5_C100.clicked.connect(self.btn_loc5_C100_clicked)
-        self.btn_loc5_C67.clicked.connect(self.btn_loc5_C67_clicked)
-        self.btn_loc5_C50.clicked.connect(self.btn_loc5_C50_clicked)
-        # Fault Location 5 - Multi-Phase Faults
-        self.btn_loc5_AB.clicked.connect(self.btn_loc5_AB_clicked)
-        self.btn_loc5_AC.clicked.connect(self.btn_loc5_AC_clicked)
-        self.btn_loc5_BC.clicked.connect(self.btn_loc5_BC_clicked)
-        self.btn_loc5_bolt.clicked.connect(self.btn_loc5_bolt_clicked)
-
-        # Fault Location 6 - Menu Functions
-        self.btn_OneLine_loc6TL6.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(6))
-        self.btn_loc6_menu.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.btn_loc6_reset.clicked.connect(self.btn_reset_clicked)
-        # Fault Location 6 - A Phase Resistive Faults
-        self.btn_loc6_A200.clicked.connect(self.btn_loc6_A200_clicked)
-        self.btn_loc6_A100.clicked.connect(self.btn_loc6_A100_clicked)
-        self.btn_loc6_A67.clicked.connect(self.btn_loc6_A67_clicked)
-        self.btn_loc6_A50.clicked.connect(self.btn_loc6_A50_clicked)
-        # Fault Location 6 - B Phase Resistive Faults
-        self.btn_loc6_B200.clicked.connect(self.btn_loc6_B200_clicked)
-        self.btn_loc6_B100.clicked.connect(self.btn_loc6_B100_clicked)
-        self.btn_loc6_B67.clicked.connect(self.btn_loc6_B67_clicked)
-        self.btn_loc6_B50.clicked.connect(self.btn_loc6_B50_clicked)
-        # Fault Location 6 - C Phase Resistive Faults
-        self.btn_loc6_C200.clicked.connect(self.btn_loc6_C200_clicked)
-        self.btn_loc6_C100.clicked.connect(self.btn_loc6_C100_clicked)
-        self.btn_loc6_C67.clicked.connect(self.btn_loc6_C67_clicked)
-        self.btn_loc6_C50.clicked.connect(self.btn_loc6_C50_clicked)
-        # Fault Location 6 - Multi-Phase Faults
-        self.btn_loc6_AB.clicked.connect(self.btn_loc6_AB_clicked)
-        self.btn_loc6_AC.clicked.connect(self.btn_loc6_AC_clicked)
-        self.btn_loc6_BC.clicked.connect(self.btn_loc6_BC_clicked)
-        self.btn_loc6_bolt.clicked.connect(self.btn_loc6_bolt_clicked)
+        self.btn_loc1_ABC.clicked.connect(self.btn_loc1_ABC_clicked)
+        self.btn_loc1_GRND.clicked.connect(self.btn_loc1_GRND_clicked)
 
     @pyqtSlot()
 # BEGIN MAIN MENU DEFINITIONS
@@ -194,6 +84,19 @@ class App(QWidget):
         # Reset All
         tx = [170, 3, 254, 129, 0, 44]
         serial_transmit(tx)
+
+    def phase_select_loc1(self):
+        if self.ui.comboBox_Loc1.currentTextChanged(0):
+            lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(0)
+        elif self.ui.comboBox_Loc1.currentTextChanged(1):
+            lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(1)
+        elif self.ui.comboBox_Loc1.currentTextChanged(2):
+            lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(2)
+        elif self.ui.comboBox_Loc1.currentTextChanged(3):
+            lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(3)
+        else:
+            lambda: self.ui.stackedWidget_Loc1.setCurrentIndex(0)
+
 # END MAIN MENU DEFINITIONS
 
 # BEGIN LOCATION 1 FAULT DEFINITIONS
@@ -242,6 +145,9 @@ class App(QWidget):
         tx = [170, 3, 254, 108, 1, 24]
         serial_transmit(tx)
 
+    def btn_loc1_Abolt_clicked(self):
+        print("Null")
+
     # Fault Location 1 - B Phase Resistive Faults
     def btn_loc1_B200_clicked(self):
         # Prep Resistor Bank
@@ -286,6 +192,9 @@ class App(QWidget):
         # Close Phase Switch
         tx = [170, 3, 254, 108, 3, 26]
         serial_transmit(tx)
+
+    def btn_loc1_Bbolt_clicked(self):
+        print("Null")
 
     # Fault Location 1 - C Phase Resistive Faults
     def btn_loc1_C200_clicked(self):
@@ -332,6 +241,9 @@ class App(QWidget):
         tx = [170, 3, 254, 108, 4, 27]
         serial_transmit(tx)
 
+    def btn_loc1_Cbolt_clicked(self):
+        print("Null")
+
     # Fault Location 1 - Multi-Phase Faults
     def btn_loc1_AB_clicked(self):
         # Phase A
@@ -357,7 +269,7 @@ class App(QWidget):
         tx = [170, 3, 254, 108, 4, 27]
         serial_transmit(tx)
 
-    def btn_loc1_bolt_clicked(self):
+    def btn_loc1_ABC_clicked(self):
         # Phase A
         tx = [170, 3, 254, 108, 1, 24]
         serial_transmit(tx)
@@ -367,6 +279,10 @@ class App(QWidget):
         # Phase C
         tx = [170, 3, 254, 108, 4, 27]
         serial_transmit(tx)
+
+    def btn_loc1_GRND_clicked(self):
+        print("Null")
+
 # END LOCATION 1 FAULT DEFINITIONS
 
 # BEGIN LOCATION 2 FAULT DEFINITIONS
